b696cc5aff1d9c9ce277ecd0f9692610256897bf
haoguangbo <haoguangbo@yeah.net>
2016-08-29 10:30:21 +0800

    add config

--- SF_Notifier.py
+++ SF_Notifier.py
@@ -1,13 +1,14 @@
 from urllib import request
 import json
+import config
 
 # 读取上次的长度记录
 with open('data', 'r') as f:
     length=int(f.read())
-    print(f.read())
+print(length)
 
 # 发送请求
-with request.urlopen('http://www.sf-express.com/sf-service-web/service/bills/925900124767/routes?app=bill&lang=sc&region=cn&translate=') as f:
+with request.urlopen('http://www.sf-express.com/sf-service-web/service/bills/'+config.ORDER_ID+'/routes?app=bill&lang=sc&region=cn&translate=') as f:
     data = f.read()
     if f.status==200:
         obj=json.loads(data.decode('utf-8'))
@@ -17,11 +18,12 @@ with request.urlopen('http://www.sf-express.com/sf-service-web/service/bills/925
         if len(routes)>length:
             length=len(routes)
             # 发送邮件
-            dosomething()
+            # dosomething()
+            print('mail!!!')
             # 更新长度记录
             with open('data', 'w') as f:
-                f.write(length)
-            print(f.read())
+                f.write(str(length))
+            # print(f.read())
 
         print(len(routes))
     # for k, v in f.getheaders():
