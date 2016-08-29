fb7bfface73c3c50340035ec2c27475a76effa11
haoguangbo <haoguangbo@yeah.net>
2016-08-29 11:14:46 +0800

    send mail done

--- SF_Notifier.py
+++ SF_Notifier.py
@@ -33,12 +33,24 @@ with request.urlopen('http://www.sf-express.com/sf-service-web/service/bills/'+c
         print(obj[0]['id'])
         routes=obj[0]['routes']
         if len(routes)>length:
-            length=len(routes)
             # 发送邮件
-            msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
-            msg['From'] = _format_addr('Python爱好者 <%s>' % config.SEND_MAIL)
-            msg['To'] = _format_addr('管理员 <%s>' % config.RECEIVE_MAIL)
-            msg['Subject'] = Header('来自SMTP的问候……', 'utf-8').encode()
+            p=''
+            i=0
+            for route in routes:
+                i+=1
+                if i<=length:
+                    continue
+                # 只从更新的部分开始输出
+                p+='%s<br>%s<hr>'%(route['scanDateTime'],route['remark'])
+            msg = MIMEText('''
+            <h3>您的顺丰速递包裹有新动态了</h3>
+            <hr>
+            <p>%s</p>
+            <a href="http://www.sf-express.com/cn/sc/dynamic_functions/waybill/#search/bill-number/%s">点此查看详情</a>
+            '''%(p,str(config.ORDER_ID)), 'html', 'utf-8')
+            msg['From'] = _format_addr('顺丰自动通知 <%s>' % config.SEND_MAIL)
+            msg['To'] = _format_addr('收货人 <%s>' % config.RECEIVE_MAIL)
+            msg['Subject'] = Header('您的顺丰速递包裹有新动态了', 'utf-8').encode()
 
             server = smtplib.SMTP(config.SMTP_SERVER, 25)
             server.set_debuglevel(1)
@@ -47,6 +59,7 @@ with request.urlopen('http://www.sf-express.com/sf-service-web/service/bills/'+c
             server.quit()
             print('mail!!!')
             # 更新长度记录
+            length=len(routes)
             with open('data', 'w') as f:
                 f.write(str(length))
             # print(f.read())
